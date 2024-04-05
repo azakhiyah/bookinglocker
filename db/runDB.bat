16e13f76e83cfca56b5401c3751c2aef3593fbb5
zakhiyaharsal <zakhiyah.arsal@akr.co.id>
2024-04-05 09:52:24 +0700

    update file runDB and UpdateFine

--- db/runDB.bat
+++ db/runDB.bat
@@ -1,5 +1,4 @@
 title HSQLDBServer
+:: change diretory where place of file hsqld.jar is stored base on your environment
 java  -cp "D:\Work_Directory\GIT_Upload\BookingLocker\db\lib\hsqldb.jar" org.hsqldb.server.Server --database.0 file:database\Locker --dbname.0 locker
 pause
-
-:: D:\Work_Directory\GIT_Upload\BookingLocker\db\lib\hsqldb.jar => rubah sesuai letak directory file hsqldb.jar berada
